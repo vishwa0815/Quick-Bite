a40480d7a2d820356149c11749031b266f22bf7f
VISHWANATH B <vishwanathbijoor100@gmail.com>
2025-07-26 12:39:32 +0530

    db.js

--- Backend/db/db.js
+++ Backend/db/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectToDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://sidduhatavadi7:sidduhatavadi7@cluster0.cqfdtwt.mongodb.net/');
+        await mongoose.connect('MONGO_URL');
         console.log('DataBase Connected Succesfully');
     } catch (error) {
         console.log(error);
